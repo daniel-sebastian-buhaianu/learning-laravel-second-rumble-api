e4f34cb417b89cb3aca7042058da036b595311a4
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-18 14:22:01 +0300

    Update user factory and database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'email' => 'tate@gmail.com',
+            'is_admin' => true,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'email' => 'admin@gmail.com',
+            'is_admin' => true,
+        ]);
+
+        \App\Models\User::factory(10)->create();
     }
 }
