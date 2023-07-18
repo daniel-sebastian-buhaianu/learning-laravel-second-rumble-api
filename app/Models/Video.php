908d8e571e340b8b0296d2483e488850524cd8dd
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-18 13:38:45 +0300

    Create migration for tag_video table; Define relationships between models

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Channel;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Video extends Model
 {
     use HasFactory;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class);
+    }
 }
