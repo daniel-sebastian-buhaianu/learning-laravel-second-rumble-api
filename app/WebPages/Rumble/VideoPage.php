7aaff4f3e63eb0c91b1d4af57b0e7a99cfc34a30
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-20 14:57:20 +0300

    Create App\WebPages\Rumble\VideoPage; implement VideoPage::__construct() and VideoPage::id() methods

---
+++ app/WebPages/Rumble/VideoPage.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\WebPages\Rumble;
+
+use \DOMXpath;
+use \DOMDocument;
+use Illuminate\Support\Facades\Cache;
+
+class VideoPage
+{
+    protected $url;
+    protected $dom = [];
+
+    public function __construct(string $url)
+    {
+        $doc = new DOMDocument();
+
+        $cacheKey = 'HTML_' . md5($url);
+
+        if (Cache::has($cacheKey)) {
+            $html = Cache::get($cacheKey);
+
+            @$doc->loadHTML($html);
+        } else {
+            @$doc->loadHTMLFile($url);
+            $html = $doc->saveHTML();
+
+            Cache::put($cacheKey, $html, now()->addHours(6));
+        }
+
+        $this->url = $url;
+        $this->dom = [
+            'doc' => $doc,
+            'xpath' => new DOMXpath($doc)
+        ];
+    }
+
+    public function id()
+    {
+        if (empty($this->url)) {
+            throw new Exception('url is empty');
+        }
+
+        return str_replace('https://rumble.com/', '', $this->url);
+    }
+}
