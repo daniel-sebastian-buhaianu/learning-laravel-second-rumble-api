fa37a47323464bd9b51cfe12528542b9f1a1060a
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-21 09:57:24 +0300

    Update VideoPage::description()

--- app/WebPages/Rumble/VideoPage.php
+++ app/WebPages/Rumble/VideoPage.php
@@ -91,6 +91,24 @@ class VideoPage
         return $videoData['name'];
     }
 
+    public function thumbnail()
+    {
+        $videoData = $this->apiData[0];
+
+        if (empty($videoData)) return null;
+
+        return $videoData['thumbnailUrl'];
+    }
+
+    public function description()
+    {
+        $videoData = $this->apiData[0];
+
+        if (empty($videoData)) return null;
+
+        return $videoData['description'];
+    }
+
     public function channelName()
     {
         $xpath = $this->dom['xpath'];
@@ -102,47 +120,6 @@ class VideoPage
         $elements = $xpath->query('//div[@class="media-heading-name"]');
 
         return ($elements->length > 0) ? trim($elements->item(0)->textContent) : null;
-    }
-
-    public function description()
-    {
-        $xpath = $this->dom['xpath'];
-
-        if (empty($xpath)) {
-            throw new Exception('xpath is empty');
-        }
-
-        // Case 1: short description
-        $elements = $xpath->query('//p[@class="media-description"]');
-
-        if ($elements->length > 0) return $elements->item(0)->textContent;
-
-        // Case 2: long description
-        $elements = $xpath->query('//p[@class="media-description media-description--first"]');
-
-        if ($elements->length > 0) {
-            $description = $elements->item(0)->textContent;
-            
-            // Remove "Show more" suffix (the last 9 characters)
-            $description = substr($description, 0, -9);
-
-            // Add a white space at the end of each string
-            $description .= ' ';
-
-            $elements = $xpath->query('//p[@class="media-description media-description--more"]');
-
-            foreach ($elements as $element) {
-                $description .= $element->textContent . ' ';
-
-                // Add a white space before "https://" if there is no white space before it already
-                $description = preg_replace('/(?<!\s)https:\/\//', ' https://', $description);
-            }
-
-            return $description;
-        }
-
-        // Case 3: no description
-        return null;
     }
 
     public function likesCount()
