4f735006985e09e708fb812d90e52a14ae8cffd3
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-23 16:38:08 +0300

    Implement CreateVideoTest::a_user_cannot_create_a_video()

--- tests/Feature/Routes/Video/CreateVideoTest.php
+++ tests/Feature/Routes/Video/CreateVideoTest.php
@@ -25,4 +25,22 @@ class CreateVideoTest extends TestCase
 
         $response->assertUnauthorized();
     }
+
+    /**
+     * @test
+     */
+    public function a_user_cannot_create_a_video(): void
+    {
+        $user = User::factory()->create();
+
+        $attributes = [
+            'url' => 'https://rumble.com/v2zndx2-andrew-tate-tucker-carlson-the-interview.html'
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Basic ' . base64_encode($user->email . ':' . 'Abc123000!'),
+        ])->post('/api/videos', $attributes);
+
+        $response->assertForbidden();
+    }
 }
