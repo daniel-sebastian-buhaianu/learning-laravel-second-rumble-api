00862681c23f2d90c2fb40e732ea16c78a9d3518
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-23 14:51:34 +0300

    Implement UpdateChannelTest::an_admin_can_update_a_channel()

--- tests/Feature/Routes/Channel/UpdateChannelTest.php
+++ tests/Feature/Routes/Channel/UpdateChannelTest.php
@@ -47,4 +47,31 @@ class UpdateChannelTest extends TestCase
         
         $response->assertForbidden();
     }
+
+    /**
+     * @test
+     */
+    public function an_admin_can_update_a_channel(): void
+    {
+        $user = User::factory()->create([
+            'is_admin' => true
+        ]);
+
+        $channel = Channel::factory()->create();
+
+        $attributes = [
+            'name' => 'A new channel name',
+            'description' => 'Cool description',
+            'banner' => 'https://img.freepik.com/free-vector/set-mixed-banners_53876-63010.jpg?w=2000',
+            'avatar' => 'https://img.freepik.com/free-vector/set-mixed-banners_53876-63010.jpg?w=2000',
+            'followers_count' => 1200,
+            'videos_count' => 65
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Basic ' . base64_encode($user->email . ':' . 'Abc123000!'),
+        ])->patch('/api/channels/' . $channel->id, $attributes);
+        
+        $response->assertJsonFragment($attributes);
+    }
 }
