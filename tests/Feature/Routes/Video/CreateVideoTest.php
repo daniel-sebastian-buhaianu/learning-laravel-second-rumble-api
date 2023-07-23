b515a311a538b96f1b6852b90dd45a91979a43bd
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-23 17:01:41 +0300

    Implement CreateVideoTest::an_admin_cannot_create_a_video_with_invalid_attributes()

--- tests/Feature/Routes/Video/CreateVideoTest.php
+++ tests/Feature/Routes/Video/CreateVideoTest.php
@@ -88,4 +88,47 @@ class CreateVideoTest extends TestCase
 
         $response->assertStatus(422);
     }
+
+    /**
+     * @test
+     * @dataProvider invalidAttributes
+     */
+    public function an_admin_cannot_create_a_video_with_invalid_attributes(array $invalidAttributes): void
+    {
+        $user = User::factory()->create([
+            'is_admin' => true
+        ]);
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Basic ' . base64_encode($user->email . ':' . 'Abc123000!'),
+        ])->post('/api/videos', $invalidAttributes);
+
+        $response->assertRedirect();
+    }
+
+    public function invalidAttributes(): array
+    {
+        return [
+            [[
+                'url' => 'https://google.com'
+            ]],
+            [[
+                'url' => 'https://rumble.com/v'
+            ]],
+            [[
+                'url' => 'https://rumble.com/v123.html'
+            ]],
+            [[
+                'url' => 'https://rumble.com/v1a3b4d-abc-defg.html'
+            ]],
+            [[
+                'url' => 'https://rumble.com/v2zndx2123-andrew-tate-tucker-carlson-the-intervi123ew.html'
+            ]],
+            [[
+                'url' => 'https://real-gibrish-url.net'
+            ]]
+        ];
+    }
+
+
 }
