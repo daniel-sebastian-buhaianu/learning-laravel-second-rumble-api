71a2d87a48f823201dab488740e6bf48a9c6fd08
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-23 15:51:11 +0300

    Implement DeleteChannelTest::a_user_cannot_delete_a_channel()

--- tests/Feature/Routes/Channel/DeleteChannelTest.php
+++ tests/Feature/Routes/Channel/DeleteChannelTest.php
@@ -23,4 +23,20 @@ class DeleteChannelTest extends TestCase
 
         $response->assertUnauthorized();
     }
+
+    /**
+     * @test
+     */
+    public function a_user_cannot_delete_a_channel(): void
+    {
+        $user = User::factory()->create();
+
+        $channel = Channel::factory()->create();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Basic ' . base64_encode($user->email . ':' . 'Abc123000!'),
+        ])->delete('/api/channels/' . $channel->id);
+
+        $response->assertForbidden();
+    }
 }
