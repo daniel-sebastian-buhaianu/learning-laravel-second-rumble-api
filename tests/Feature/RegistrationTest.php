2d3dae1ed3f9f15e034f092b8664aa329953a412
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-23 09:24:01 +0300

    Create and implement RegistrationTest

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function guest_can_register_with_valid_credentials(): void
+    {
+        $attributes = [
+            'email' => 'test@test.com',
+            'password' => 'Abc123000!'
+        ];
+
+        $this->post('api/register', $attributes);
+
+        $this->assertDatabaseHas('users', ['email' => $attributes['email']]);
+    }
+
+    /** @test */
+    public function guest_cannot_register_with_invalid_email(): void
+    {
+        $attributes = [
+            'email' => 'test.com',
+            'password' => 'Abc123000!'
+        ];
+
+        $this->post('api/register', $attributes);
+
+        $this->assertDatabaseMissing('users', ['email' => $attributes['email']]);
+    }
+
+    /** @test */
+    public function guest_cannot_register_with_weak_password(): void
+    {
+        $attributes = [
+            'email' => 'test@test.com',
+            'password' => 'Abc123000'
+        ];
+
+        $this->post('api/register', $attributes);
+
+        $this->assertDatabaseMissing('users', ['email' => $attributes['email']]);
+    }
+}
