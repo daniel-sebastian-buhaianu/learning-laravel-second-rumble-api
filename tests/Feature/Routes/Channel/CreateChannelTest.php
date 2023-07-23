f56a9564976a570e01626ba5af2770ab83150a3b
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-23 12:44:47 +0300

    Implement CreateChannelTest::a_user_cannot_create_a_channel()

--- tests/Feature/Routes/Channel/CreateChannelTest.php
+++ tests/Feature/Routes/Channel/CreateChannelTest.php
@@ -25,4 +25,22 @@ class CreateChannelTest extends TestCase
 
         $response->assertUnauthorized();
     }
+
+    /**
+     * @test
+     */
+    public function a_user_cannot_create_a_channel(): void
+    {
+        $user = User::factory()->create();
+
+        $attributes = [
+            'url' => 'https://rumble.com/c/tateconfidential'
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Basic ' . base64_encode($user->email . ':' . 'Abc123000!'),
+        ])->post('/api/channels', $attributes);
+
+        $response->assertForbidden();
+    }
 }
