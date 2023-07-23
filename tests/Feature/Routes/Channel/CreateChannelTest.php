35b1a63c2efc7f6e2ed32d49a7645908aacf199f
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-07-23 12:37:53 +0300

    Implement CreateChannelTest::a_guest_cannot_create_a_channel()

---
+++ tests/Feature/Routes/Channel/CreateChannelTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Routes\Channel;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Channel;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateChannelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function a_guest_cannot_create_a_channel(): void
+    {
+        $attributes = [
+            'url' => 'https://rumble.com/c/tateconfidential'
+        ];
+
+        $response = $this->post('api/channels', $attributes);
+
+        $response->assertUnauthorized();
+    }
+}
